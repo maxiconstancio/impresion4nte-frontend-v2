418c54c353f647c6a4cbf9d2037fd6e2bd9db9a7
maxiconstancio <maxiconstancio@gmail.com>
2025-05-25 23:41:15 -0300

    second commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  timeout: 10000
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
 });
 
 export default api;
